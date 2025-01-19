5a77db12a37c98031b81a0854a1e59802d2d818c
amit <mr.ahr.es6@gmail.com>
2025-01-19 11:11:45 +0600

    text select option added

--- src/ActionClassTwo.java
+++ src/ActionClassTwo.java
@@ -9,18 +9,21 @@ public class ActionClassTwo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		WebDriver driver = new ChromeDriver();
-		
-		driver.get("https://www.google.co.uk/");
-		
-		Actions action = new Actions(driver);
-		
-	WebElement searchBox =	driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
-	
-	action.moveToElement(searchBox).click()
-	.keyDown(Keys.SHIFT).sendKeys("hello world").build().perform();
 
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.google.co.uk/");
+
+		Actions action = new Actions(driver);
+
+		WebElement searchBox = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
+        // to write in capital letter
+		action.moveToElement(searchBox).click().keyDown(Keys.SHIFT).sendKeys("hello world").build().perform();
+
+		// to select using double click 
+		action.moveToElement(searchBox).doubleClick().build().perform();
+		
+		
 	}
 
 }
